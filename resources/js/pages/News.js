8f768c985c060c4d5b9d84f7757f6c04243411d4
MaximBrewer <pimax1978@icloud.com>
2021-02-20 17:06:29 +0600

    fix

--- resources/js/pages/News.js
+++ resources/js/pages/News.js
@@ -6,25 +6,37 @@ function News() {
     <section id="news" className="bg-white xl:bg-transparent mt-80 xl:mt-8">
       <div className="w-full h-px"></div>
       {window.App.data.news.map((item, index) => (
-        <div className={`xl:py-12 ${index ? `bg-white` : `text-white`}`} key={index}>
+        <div
+          className={`xl:py-12 ${index ? `bg-white` : `-mt-64 xl:mt-0`}`}
+          key={index}
+        >
           <div className="max-w-sm xl:max-w-full mx-auto">
             <div className="container mx-auto px-4">
-              <div className="xl:flex justify-between items-center">
+              <div className="flex flex-col xl:flex-row justify-between items-center">
                 <div
-                  className={`xl:mt-0 xl:w-3/5 xl:order-${index % 2 ? 1 : 2}`}
+                  className={`xl:mt-0 xl:w-3/5 order-2 xl:order-${
+                    index % 2 ? 1 : 2
+                  }`}
                 >
                   <h2
-                    className={`text-2xl font-bold text-center xl:text-left`}
+                    className={`hidden xl:block text-2xl font-bold text-center xl:text-left`}
                   >
                     {item.title}
                   </h2>
                   <div className="my-4 text-xl">{Parser(item.body)}</div>
                 </div>
                 <div
-                  className={`xl:w-2/5 xl:${
+                  className={`w-full xl:w-2/5 xl:${
                     index % 2 ? "pl" : "pr"
-                  }-24 xl:order-${index % 2 ? 2 : 1}`}
+                  }-24 order-1 xl:order-${index % 2 ? 2 : 1}`}
                 >
+                  <h2
+                    className={`xl:hidden text-2xl font-bold text-center ${
+                      !index ? `text-white` : ``
+                    }`}
+                  >
+                    {item.title}
+                  </h2>
                   <div className="my-4">
                     <div
                       className="border-7 border-yellow-900 rounded-3xl overflow-hidden relative bg-center bg-no-repeat bg-cover w-full pb-100%"
