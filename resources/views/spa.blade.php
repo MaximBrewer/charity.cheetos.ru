3555f1994e2274a3559ea85955317d6459ba188e
MaximBrewer <pimax1978@icloud.com>
2021-03-01 21:06:09 +0600

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -22,14 +22,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('fonts/stylesheet.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css?v1.7') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v1.9') }}" rel="stylesheet">
     
 </head>
 
 <body className="font-sans h-full text-grey-darkest">
     <div id="app" class="flex flex-col select-none">
     </div>
-    <script src="{{ asset('js/app.js?v1.8') }}" defer></script>
+    <script src="{{ asset('js/app.js?v1.9') }}" defer></script>
 </body>
 
 </html>
