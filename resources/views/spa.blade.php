e71af30fda5e444deea873ae620010088a388887
MaximBrewer <pimax1978@icloud.com>
2021-03-11 16:50:46 +0600

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -22,14 +22,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('fonts/stylesheet.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css?v1.15') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v1.16') }}" rel="stylesheet">
     
 </head>
 
 <body className="font-sans h-full text-grey-darkest">
     <div id="app" class="flex flex-col select-none">
     </div>
-    <script src="{{ asset('js/app.js?v1.15') }}" defer></script>
+    <script src="{{ asset('js/app.js?v1.16') }}" defer></script>
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
+  purge: false,
   theme: {
     fontSize: {
       xs: ".75rem",
