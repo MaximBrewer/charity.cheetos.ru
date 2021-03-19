046fb0a7f9f1a0bd47452fbad1bc0b696754eb61
MaximBrewer <pimax1978@icloud.com>
2021-03-19 18:31:37 +0600

    phone

--- resources/views/exports/trips.blade.php
+++ resources/views/exports/trips.blade.php
@@ -5,6 +5,7 @@
             <td>Город</td>
             <td>Кол-во</td>
             <td>E-mail</td>
+            <td>Телефон</td>
             <td>Дата/время</td>
         </tr>
     </thead>
@@ -15,6 +16,7 @@
                 <td>{{ $trip->city->title }}</td>
                 <td>{{ $trip->quantity }}</td>
                 <td>{{ $trip->email }}</td>
+                <td>{{ $order->phone }}</td>
                 <td>{{ \Illuminate\Support\Carbon::parse($trip->created_at)->format('d.m.Y H:i') }}</td>
             </tr>
         @endforeach
